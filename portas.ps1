e9972cf8ec69771734c89d1edfa481ff48e431da
Daniel Angelo <102752827+DnzxDevop@users.noreply.github.com>
2025-01-30 23:24:27 -0300

    Update portas.ps1

--- portas.ps1
+++ portas.ps1
@@ -4,8 +4,6 @@ $ports = 3000..3010
 foreach ($port in $ports) {
     $ruleName = "Abrir Porta HTTP $port"
     Write-Output "Abrindo porta $port no firewall..."
-    
-    # Adiciona a regra ao firewall
     New-NetFirewallRule -DisplayName $ruleName -Direction Inbound -Protocol TCP -LocalPort $port -Action Allow
 }
 
